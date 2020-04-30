67d6ce7e51f7cd155c4b2c148cdf5c81eda4bb7e
Your Name <you@example.com>
2020-04-30 11:49:38 +0900

    My Django Girls app, first commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path 
 from . import views
 
 urlpatterns = [
@@ -6,5 +6,4 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
+from django.shortcuts import redirect
 
 
 
@@ -15,7 +15,16 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})    
 
 def post_new(request):
-    form = PostForm()
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_edit(request, pk):
